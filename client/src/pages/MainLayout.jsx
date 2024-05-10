27cfb8e614c154592baef4dfb70a1bb4d279a9e9
mahedi066 <imam25349@gmail.com>
2024-05-10 11:46:32 +0600

    font family added

--- client/src/pages/MainLayout.jsx
+++ client/src/pages/MainLayout.jsx
@@ -5,7 +5,7 @@ import Footer from "../components/Footer";
 
 const MainLayout = () => {
     return (
-        <div >
+        <div className="font-poppins">
             {/* navbar  */}
             <Navbar/>
             {/* outlate */}
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
     return (
         <div className="container mx-auto">
-            <h1 className="text-6xl font-bold ">this is home</h1>
+            <h1 className="text-6xl font-bold font-poppins uppercase">this is home</h1>
         </div>
     );
 };
